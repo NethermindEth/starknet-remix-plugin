e8f9a8e1760d46bd7a55cfd6e07229ec9c5e558d
Edgar Barrantes <edgar@barrantes.dev>
2023-03-23 17:52:47 -0600

    Remove space in card

--- plugin/src/components/Card/index.tsx
+++ plugin/src/components/Card/index.tsx
@@ -10,11 +10,6 @@ export const Card: React.FC<CardProps> = ({ header, children }) => {
     <div className="border-top border-bottom">
       <div className="card-header">
         <h5 className="mb-0">{header}</h5>
-        <i
-          className="ml-2 fal fa-info-circle align-self-center"
-          aria-hidden="true"
-          style={{ fontSize: "medium" }}
-        ></i>
       </div>
       <div className="card-body">{children}</div>
     </div>
