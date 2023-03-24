251b8624d2948f374ecab0c72b79235a8907d64d
Edgar Barrantes <edgar@barrantes.dev>
2023-03-24 09:46:08 -0600

    Add wallet connection

--- plugin/src/App.tsx
+++ plugin/src/App.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@remixproject/plugin-webview";
 import { PluginClient } from "@remixproject/plugin";
-import { Provider } from "starknet";
+import { InjectedConnector, StarknetConfig } from "@starknet-react/core";
+
 import { useEffect, useState } from "react";
 
 import "./App.css";
@@ -8,28 +9,28 @@ import "./App.css";
 import Nav from "./components/Nav";
 
 const remixClient = createClient(new PluginClient());
-
 function App() {
-  // Use state variable cairoVersion
-  const [cairoVersion, setCairoVersion] = useState("1.0.0-alpha.4");
-  // Call the API and make the api return the version of the Cairo compiler on use effect
+  const [cairoVersion, setCairoVersion] = useState("1.0.0-alpha.6");
+  const connectors = [
+    new InjectedConnector({ options: { id: "argentX" } }),
+    new InjectedConnector({ options: { id: "braavos" } }),
+  ];
   useEffect(() => {
-    setCairoVersion("1.0.0-alpha.4");
+    // Call the API and make the api return the version of the Cairo compiler on use effect
+    setCairoVersion("1.0.0-alpha.6");
   }, []);
 
-  const provider = new Provider({
-    sequencer: { baseUrl: "http://127.0.0.1:5050" },
-  });
-
   return (
-    <div className="shell">
-      <div className="mb-1">
-        <label className="cairo-version-legend">
-          Using cairo version {cairoVersion}
-        </label>
+    <StarknetConfig connectors={connectors}>
+      <div className="shell">
+        <div className="mb-1">
+          <label className="cairo-version-legend">
+            Using cairo version {cairoVersion}
+          </label>
+        </div>
+        <Nav remixClient={remixClient}></Nav>
       </div>
-      <Nav remixClient={remixClient}></Nav>
-    </div>
+    </StarknetConfig>
   );
 }
 
