719514e39a53584472f68e499680353aa973346a
Edgar Barrantes <edgar@barrantes.dev>
2023-04-14 12:17:56 -0600

    Add licenses

--- plugin/src/utils/constants.ts
+++ plugin/src/utils/constants.ts
@@ -16,10 +16,33 @@ const networks = [
   { name: "Mainnet", value: "mainnet-alpha" },
 ];
 
+const networkExplorerUrls = {
+  "goerli-alpha": "https://goerli.voyager.online",
+  "goerli-alpha-2": "https://goerli-2.voyager.online",
+  "mainnet-alpha": "https://voyager.online",
+};
+
 const networkEquivalents = new Map([
   [constants.StarknetChainId.SN_GOERLI, "goerli-alpha"],
   [constants.StarknetChainId.SN_GOERLI2, "goerli-alpha-2"],
   [constants.StarknetChainId.SN_MAIN, "mainnet-alpha"],
 ]);
 
-export { connectors, devnetUrl, networkEquivalents, networks };
+const licenses = [
+  "No License (None)",
+  "The Unlicense (Unlicense)",
+  "MIT License (MIT)",
+  "GNU General Public License v2.0 (GNU GPLv2)",
+  "GNU General Public License v3.0 (GNU GPLv3)",
+  "GNU Lesser General Public License v2.1 (GNU LGPLv2.1)",
+  "GNU Lesser General Public License v3.0 (GNU LGPLv3)",
+  'BSD 2-clause "Simplified" license (BSD-2-Clause)',
+  'BSD 3-clause "New" Or "Revisited license (BSD-3-Clause)',
+  "Mozilla Public License 2.0 (MPL-2.0)",
+  "Open Software License 3.0 (OSL-3.0)",
+  "Apache 2.0 (Apache-2.0)",
+  "GNU Affero General Public License (GNU AGPLv3)",
+  "Business Source License (BSL 1.1)",
+];
+
+export { connectors, devnetUrl, networkEquivalents, networks, licenses };
