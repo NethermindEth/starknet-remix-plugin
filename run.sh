3f0c819ad874834a9d772b69648296b04f1f80e1
Rohit Ranjan <rohitrjn629@gmail.com>
2023-06-12 11:43:30 +0530

    add run.sh file

--- run.sh
+++ run.sh
@@ -1,30 +1,17 @@
-sudo apt-get update
-sudo  apt-get install -y curl
-sudo  apt-get install -y git
-sudo  apt-get install -y software-properties-common
-sudo  apt-get install -y libgmp-dev
+git clone git@github.com:NethermindEth/starknet-remix-plugin.git
 
+cd starknet-remix-plugin/plugin
 
-sudo add-apt-repository ppa:deadsnakes/ppa -y
+pnpm install
 
-# Install python3.9 and pip3.9
-sudo  apt install -y python3.9 python3.9-dev python3.9-distutils python3.9-venv
-sudo  apt-get install -y python3-pip
+screen -S starknet-remix-frontend -d -m pnpm run start
 
-# Install Rust
-sudo  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
+cd ../api
 
+git submodule update --init;
+cargo build;
 
-# Install nodejs
-sudo  curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo  apt-get install -y nodejs
+screen -S rust-backend -d -m cargo run
 
-# Install pnpm
-sudo  npm install -g pnpm
-
-python3.9 -m venv starknet-venv
-source starknet-venv/bin/activate
-pip3 install starknet-devnet
-
-deactivate
+source ../../starknet-venv/bin/activate
+screen -S starknet-devnet -d -m starknet-devnet --port 0.0.0.0
