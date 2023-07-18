a22f06491bd95eb5b851ba95660c6920fd860933
Anish gehlot <anishgehlot@gmail.com>
2023-07-18 13:06:50 +0530

    Add IaC

--- IaC/ecs/003-task-definition/variables.tf
+++ IaC/ecs/003-task-definition/variables.tf
@@ -17,47 +17,7 @@ variable "owner" {
 }
 
 
-##################################### ECS Task and Service Variables 
-/*
-variable "task_defination_family" {
-  type        = string
-  default     = "angkor-task-defination-family-name"
-  description = "name for the angkor task defination"
-}
-
-variable "image" {
-  type        = string
-  description = "container image to deploy"
-}
-
-variable "task_memory" {
-  type        = number
-  default     = 512
-  description = "memory allocated for task"
-}
-
-variable "task_cpu" {
-  type        = number
-  default     = 512
-  description = "cpu allocated for task"
-}
-
-variable "container_name" {
-  type        = string
-  default     = "angkor"
-  description = "Container name should be belong to task defination of service"
-}
-
-variable "container_port" {
-  type        = number
-  default     = 8065
-  description = "exposed port of service"
-}
-
-variable "requires_compatibilities" {
-  type    = list(string)
-  default = ["FARGATE"]
-}*/
+##################################### ECS Task and Service Variables check locals.tf file
 ###########################################################CloudWatch Variables
 
 variable "ecs_log_stream_prefix" {
