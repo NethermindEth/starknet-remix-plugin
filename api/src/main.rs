cadfced954da7693b5fae3bcbfb86a5fb9fe7c4e
Rohit Ranjan <rohitrjn629@gmail.com>
2023-09-12 15:17:23 +0530

    cors origin change

--- api/src/main.rs
+++ api/src/main.rs
@@ -39,8 +39,7 @@ impl Fairing for CORS {
 
         response.set_header(Header::new(
             "Access-Control-Allow-Origin",
-            // "https://cairo-remix-test.nethermind.io"
-            "*",
+            "https://cairo-remix-test.nethermind.io"
         ));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
     }
