2b42e8c7b9773db1d59eb7b0bd199131d82422f9
varex83 <limposfeed@gmail.com>
2023-10-18 21:29:06 +0300

    implement map purging

--- api/src/utils/lib.rs
+++ api/src/utils/lib.rs
@@ -16,7 +16,7 @@ pub const TEMP_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/", "temp/");
 
 pub const DEFAULT_CAIRO_VERSION: &str = "v2.2.0";
 
-pub const DURATION_TO_PURGE: u64 = 5; // 5 seconds
+pub const DURATION_TO_PURGE: u64 = 60 * 5; // 5 minutes
 
 pub fn get_file_ext(file_path: &str) -> String {
     match file_path.split('.').last() {
