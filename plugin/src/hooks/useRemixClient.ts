a9ce06f91e879fafb889ce5a96ae9a1f65709abe
Rohit Ranjan <rohitrjn629@gmail.com>
2024-02-16 11:33:26 +0530

    implement test functions

--- plugin/src/hooks/useRemixClient.ts
+++ plugin/src/hooks/useRemixClient.ts
@@ -1,5 +1,7 @@
 import { PluginClient } from '@remixproject/plugin'
 import { createClient } from '@remixproject/plugin-webview'
+import { fetchGitHubFilesRecursively } from '../utils/initial_scarb_codes'
+import axios from 'axios'
 
 export class RemixClient extends PluginClient {
   constructor () {
@@ -7,12 +9,46 @@ export class RemixClient extends PluginClient {
     this.methods = ['loadFolderFromUrl']
   }
 
-  loadFolderFromUrl (url: string, filePath: string | undefined): void {
-    console.log('loadFolderFromUrl', url, filePath)
+  async loadFolderFromUrl (url: string, filePath: string | undefined): Promise<void> {
+    try {
+      await this.call('filePanel', 'createWorkspace', 'code-sample', false)
+      // Fetch JSON data from the URL
+      const response = await axios.get(url)
+      const folderContent = response.data
+
+      // Check if filePath is provided, if not, set it to an empty string
+      filePath = filePath ?? ''
+
+      // Iterate over each file in the folderContent
+      for (const [fileName, fileContent] of Object.entries(folderContent)) {
+        // Construct the full file path
+        const fullFilePath = `${filePath}/${fileName}`
+        await this.call('fileManager', 'setFile', fullFilePath, fileContent as string)
+      }
+
+      console.log('Folder loaded successfully.')
+    } catch (error) {
+      console.error('Error loading folder:', error)
+    }
   }
 
-  loadFolderFromGithub (url: string, filePath: string | undefined): void {
-    console.log('loadFolderFromGithub', url, filePath)
+  async loadFolderFromGithub (url: string, folderPath: string, filePath: string | undefined): Promise<void> {
+    console.log('loadFolderFromGithub', url, folderPath, filePath)
+    try {
+      await this.call('filePanel', 'createWorkspace', 'code-sample', false)
+      const folder = await fetchGitHubFilesRecursively(url, folderPath)
+      console.log('folder', folder)
+      for (const file of folder) {
+        if (file !== null) {
+          await this.call('fileManager', 'setFile', file.path, file.content)
+        }
+      }
+      if (filePath !== undefined) {
+        await this.call('fileManager', 'switchFile', filePath)
+      }
+    } catch (error) {
+      console.error('Error loading folder from GitHub:', error)
+    }
   }
 }
 const remixClient = createClient(new RemixClient())
