9f37a9dcdc45ab574bf7a880fc0a4d04adbee5ee
Bohdan Ohorodnii <limposfeed@gmail.com>
2025-03-26 17:36:00 +0200

    fix: fix connnect modal

--- plugin/src/components/starknet/connect.tsx
+++ plugin/src/components/starknet/connect.tsx
@@ -1,15 +1,14 @@
-import { Button, Flex, Heading, Image } from "@chakra-ui/react";
 import { type Connector, useConnect } from "@starknet-react/core";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
 	type StarknetkitConnector,
 	useStarknetkitConnectModal
 } from "starknetkit";
 import { availableConnectors } from "./connectors";
+import { MdAccountBalanceWallet } from "react-icons/md";
 
 const ConnectModal = (): React.ReactElement => {
-	const { connectAsync, connectors } = useConnect();
-	const [isClient, setIsClient] = useState(false);
+	const { connectAsync } = useConnect();
 
 	const { starknetkitConnectModal } = useStarknetkitConnectModal({
 		connectors: availableConnectors as StarknetkitConnector[]
@@ -19,18 +18,6 @@ const ConnectModal = (): React.ReactElement => {
 	// starknet react had an issue with the `available` method
 	// need to check their code, probably is executed only on client causing an hydration issue
 
-	useEffect(() => {
-		setIsClient(true);
-	}, []);
-
-	if (!isClient) {
-		return <></>;
-	}
-
-	const handleConnectorClick = async (connector: Connector): Promise<void> => {
-		await connectAsync({ connector });
-	};
-
 	const handleStarknetkitClick = async (): Promise<void> => {
 		const { connector } = await starknetkitConnectModal();
 		if (connector == null) return; // or throw error
@@ -38,72 +25,28 @@ const ConnectModal = (): React.ReactElement => {
 	};
 
 	return (
-		<Flex direction="column" gap="3" p="5">
-			<Flex direction="column" gap="3">
-				{connectors.map((connector: Connector) => {
-					if (!connector.available()) {
-						return <React.Fragment key={connector.id} />;
-					}
-					const icon =
-						typeof connector.icon === "string"
-							? connector.icon
-							: (connector.icon.dark ?? "");
-					const isSvg = icon?.startsWith("<svg");
-
-					return (
-						<Button
-							as="button"
-							colorScheme="neutrals"
-							key={connector.id}
-							onClick={() => {
-								handleConnectorClick(connector).catch(console.error);
-							}}
-							alignItems="center"
-							justifyContent="flex-start"
-							cursor="pointer"
-							maxW="350px"
-							gap="2"
-							py="2"
-							px="4"
-						>
-							{isSvg
-								? (
-									<div dangerouslySetInnerHTML={{ __html: icon }} />
-								)
-								: (
-									<Image
-										alt={connector.name}
-										src={icon}
-										height="32px"
-										width="32px"
-									/>
-								)}
-							{connector.name}
-						</Button>
-					);
-				})}
-			</Flex>
-
-			<Heading as="h2" mt="8">
-				Starknetkit modal + starknet-react
-			</Heading>
-			<Button
-				colorScheme="neutrals"
+		<div style={{ display: "flex", flexDirection: "column" }}>
+			<button
 				onClick={() => {
 					handleStarknetkitClick().catch(console.error);
 				}}
-				alignItems="center"
-				justifyContent="flex-start"
-				cursor="pointer"
-				maxW="350px"
-				gap="2"
-				py="2"
-				px="4"
-				mt="2"
+				style={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "flex-start",
+					cursor: "pointer",
+					gap: "0.5rem",
+					padding: "8px 16px",
+					backgroundColor: "var(--bgPrimary)",
+					border: "1px solid var(--secondary)",
+					borderRadius: "4px",
+					fontSize: "14px"
+				}}
 			>
-				Starknetkit modal with starknet-react
-			</Button>
-		</Flex>
+				<MdAccountBalanceWallet size={24} />
+				Connect with Starknetkit
+			</button>
+		</div>
 	);
 };
 
