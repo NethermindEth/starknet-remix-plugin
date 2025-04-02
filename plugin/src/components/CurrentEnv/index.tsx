0091eed04ed1f84423700a355d99272fc7b52692
om santoshwar <omsantoshwar02@gmail.com>
2025-04-02 19:01:38 +0530

    fix lint errors

--- plugin/src/components/CurrentEnv/index.tsx
+++ plugin/src/components/CurrentEnv/index.tsx
@@ -16,7 +16,6 @@ export const CurrentEnv: React.FC = () => {
 	const selectedAccountDevnet = useAtomValue(selectedDevnetAccountAtom);
 	const walletAccount = useAtomValue(account);
 	const walletBalance = useAtomValue(walletBalanceAtom);
-	console.log("walletBalance", walletBalance);
 
 	const selectedAccount =
 		env === "wallet"
@@ -40,7 +39,6 @@ export const CurrentEnv: React.FC = () => {
 		selectedAccount.address != null
 			? getShortenedHash(selectedAccount.address, 6, 4)
 			: "No account selected";
-	console.log("sdfdfd", selectedAccount.balance);
 
 	const selectedAccountBalance = ethers.utils.formatEther(selectedAccount.balance ?? 0);
 
